aa77492d5554d72f97a02e47f2534234a917858c
poltop20 <poltop20@yahoo.com>
2022-11-10 20:22:49 -0500

    updated code

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,8 +17,11 @@ public class Greeter {
    */
   public final String greet(final String someone) {
     String password = "Admin@12345";
-    System.out.printIn("Hello");
+    System.out.println("Hello");
 
+   
     return String.format("Hello Avinash, %s!", someone);
+    
   }
+  
 }
